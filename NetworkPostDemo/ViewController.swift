e4632dd46503533c01f652bd2faa299c8a26fc42
baksha97 <baksha97@gmail.com>
2018-04-05 14:00:06 -0400

    initial commit

--- NetworkPostDemo/ViewController.swift
+++ NetworkPostDemo/ViewController.swift
@@ -12,7 +12,32 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = URL(string: "https://api.lykapp.com/lykjwt/index.php?/relatedData/getHomeFeed")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        //Param: {"userId":"3196",  "offset": 0, "limit":25 }
+        let postString = "userId=3196&offset=0&limit=25"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
+                print("error=\(String(describing: error))")
+                return
+            }
+            
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(httpStatus)")
+                
+                let responseString = String(data: data, encoding: .utf8)
+                print("responseString = \(String(describing: responseString))")
+            }
+            
+            let json = try! JSONSerialization.jsonObject(with: data, options: [])
+            print(json)
+        }
+        task.resume()  
+        
     }
 
     override func didReceiveMemoryWarning() {
